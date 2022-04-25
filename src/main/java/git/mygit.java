61c8b2ea6a70539b5c06083cf01b71df588e65e7
yjx <yjx@qq.com>
2022-04-25 20:14:08 +0800

    hot-fix commit1

--- src/main/java/git/mygit.java
+++ src/main/java/git/mygit.java
@@ -4,5 +4,6 @@ public class mygit {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("bbbb");
+        System.out.println("bbbb");
     }
 }
